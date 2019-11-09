f4e8c8837eb8bac083ede2d02abb0c014658a883
Jakub Krzemień <jkrzemien28@gmail.com>
2019-11-09 12:51:11 +0100

    init

---
+++ src/main/java/pl/longhorn/rebETL/RebEtlApplication.java
@@ -0,0 +1,13 @@
+package pl.longhorn.rebETL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebEtlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebEtlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/longhorn/rebETL/RebEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.longhorn.rebETL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebEtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
